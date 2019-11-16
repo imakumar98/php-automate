cff20eeaa1b544b435a877f85d87f69f1409cec0
Ashwani <imakumar98@gmail.com>
2019-11-16 18:36:59 +0530

    Adds docs of csv to array function

---
+++ functions/csv-to-array.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     * This function returns two dimensional array by taking csv as input
+     * Parameters:
+     * - CSV file
+     * - Array of ordered column
+     * Date: 16 November 2019
+     */
+
+
+
+    //Function Implementation
+
+    function csv_to_array($csv_file, $column_array){
+
+    }
+
+
+?>
